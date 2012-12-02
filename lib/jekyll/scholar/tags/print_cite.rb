a682fca6235db6d030083e2e5a46cb9e121bc3a0
Georgios Gousios <gousiosg@gmail.com>
2012-12-02 18:25:45 +0100

    Tag to print a formatted citation

---
+++ lib/jekyll/scholar/tags/print_cite.rb
@@ -0,0 +1,43 @@
+module Jekyll
+  class Scholar
+
+    class PrintCiteTag < Liquid::Tag
+      include Scholar::Utilities
+
+      attr_reader :key, :file
+
+      def initialize(tag_name, arguments, tokens)
+        super
+
+        @config = Scholar.defaults.dup
+        @key, @file = arguments.strip.split(/\s*,\s*/, 2)
+      end
+
+      def render(context)
+        set_context_to context
+        bib = unless file.nil?
+                BibTeX.open(file, { :filter => :latex })
+              else
+                set_context_to context
+                bibliography
+              end
+
+        entry = bib[key]
+
+        if bib.key?(key)
+          CiteProc.process entry.to_citeproc,
+                           :style => config['style'],
+                           :locale => config['locale'],
+                           :format => 'html'
+        else
+          "(missing reference)"
+        end
+      rescue
+        "(#{key})"
+      end
+    end
+
+  end
+end
+
+Liquid::Template.register_tag('print_cite', Jekyll::Scholar::PrintCiteTag)
