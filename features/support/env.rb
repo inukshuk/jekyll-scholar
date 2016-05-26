93cb79981f01b1faaed5cd5f092ab3441d6922f7
Joost van Pinxten <j.h.h.v.pinxten@tue.nl>
2016-05-26 23:03:39 +0200

    Allow the test environment to keep the originally sat 'source' value.

--- features/support/env.rb
+++ features/support/env.rb
@@ -22,8 +22,16 @@ require 'jekyll/scholar'
 TEST_DIR = File.join('/', 'tmp', 'jekyll')
 
 def run_jekyll(options = {})
-
+  original_options = Jekyll.configuration()
   options['source'] ||= TEST_DIR
+  if original_options.key?('source')
+      orig_path = Pathname(original_options['source'])
+      # skip prepending the TEST_DIR environment when the path from the configuration file is absolute
+      # note: the file path is also absolute when the source has been set previously
+      if not orig_path.absolute? 
+          options['source'] = File.join TEST_DIR, original_options['source']
+      end
+  end
   options['destination'] ||= File.join(TEST_DIR, '_site')
 
   options = Jekyll.configuration(options)
