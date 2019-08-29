319ecac16c17e25b1234fdf03b0411034b574521
rmrf <nospam@uwaterloo.ca>
2019-08-28 20:33:12 -0400

    Remove print statement.

--- lib/jekyll/scholar/plugins/textit.rb
+++ lib/jekyll/scholar/plugins/textit.rb
@@ -4,7 +4,6 @@ module Jekyll
       def apply(value)
         # Use of \g<1> pattern back-reference to allow for capturing nested {} groups.
         # The first (outermost) capture of $1 is used.
-        puts "Textit"
         value.to_s.gsub(/\\textit(\{(?:[^{}]|\g<1>)*\})/) {
           "<i>#{$1[1..-2]}</i>"
         }
