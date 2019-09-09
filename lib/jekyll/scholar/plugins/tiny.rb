0062fae99f7844cf666a6cb5d11e34b6427ca8a9
Sylvester Keil <sylvester@keil.or.at>
2019-09-09 12:10:19 +0200

    Fix syntax

--- lib/jekyll/scholar/plugins/tiny.rb
+++ lib/jekyll/scholar/plugins/tiny.rb
@@ -5,7 +5,7 @@ module Jekyll
         # Use of \g<1> pattern back-reference to allow for capturing nested {} groups.
         # The first (outermost) capture of $1 is used.
         value.to_s.gsub(/\\tiny(\{(?:[^{}]|\g<1>)*\})/) {
-          "<span class="tiny">#{$1[1..-2]}</span>"
+          "<span class=\"tiny\">#{$1[1..-2]}</span>"
         }
       end
     end
